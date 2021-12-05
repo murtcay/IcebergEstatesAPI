93f0a20f9af5f252c0c6ac62a9cbc60a962080ce
Murat ÇAY <murtcay@gmail.com>
2021-12-05 17:07:01 +0300

    login endpoint added with user credential controls

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 const { createJWT } = require('../utils');
 
 const register = async (req, res) => { 
@@ -15,12 +15,7 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user'
 
   const user = await User.create({ name, email, password, role });
-  const tokenUser = {
-    name: user.name,
-    userId: user._id,
-    role: user.role
-  };
-
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
   const token = createJWT({ payload: tokenUser });
 
   res.status(StatusCodes.CREATED).json({
@@ -29,7 +24,33 @@ const register = async (req, res) => {
   });
 
 }
-const login = async (req, res) => { res.send('Login Route'); }
+const login = async (req, res) => { 
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    throw new BadRequestError('Please provide email and pasword.');
+  }
+
+  const user = await User.findOne({ email });
+
+  if(!user) {
+    throw new UnauthenticatedError('Invalid Credentials.');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if(!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials.');
+  }
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.OK).json({
+    user: tokenUser,
+    token
+  });
+}
 const logout = async (req, res) => { res.send('Logout Route'); }
 
 module.exports = {

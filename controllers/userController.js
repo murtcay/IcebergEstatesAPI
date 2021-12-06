f686f91904b4e6c0d9bdabc81cc8d10f0959d020
Murat ÇAY <murtcay@gmail.com>
2021-12-06 13:24:58 +0300

    get single user by id and get all users with user role is user. No authentication.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const getAllUsers = async (req, res) => {
-  res.send('get all users');
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send('get single user');
+  const user = await User.findOne({ _id: req.params.id }).select('-password');
+  if(!user) {
+    throw new CustomError.BadRequestError(`No user with id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const updateUser = async (req, res) => {
